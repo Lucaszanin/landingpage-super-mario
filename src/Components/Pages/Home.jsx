42e03fe51b2247de123856072c7e2db44e037fc8
Lucas Zanin <lucas.zanin@agenciam3.com>
2023-01-27 08:46:04 -0300

    Layout desktop pront

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import Header from "../Partials/Header";
+import styles from "./Home.module.css";
+import titulo from "../../Assets/titulo.png";
+import mario from "../../Assets/super-mario-chars.png";
+import video from "../../video/video-mario.mp4";
 
 function Home() {
   return (
     <div>
+      <div className={styles.backgroundVideo}>
+        <video className={styles.video} autoPlay loop muted>
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
       <Header />
+      <main className={`${styles.main} container`}>
+        <section className={`${styles.contentLeft}`}>
+          <img src={titulo} alt="Titulo do filme" />
+          <p>
+            Mario é um encanador junto com seu irmão Luigi. Um dia, eles vão
+            parar no reino dos cogumelos, governado pela Princesa Peach, mas
+            ameaçado pelo rei dos Koopas, que faz de tudo para conseguir reinar
+            em todos os lugares.
+          </p>
+        </section>
+        <section className={styles.contentRight}>
+          <img src={mario} alt="Imagem da turma do Super Mario" />
+        </section>
+      </main>
     </div>
   );
 }
